d3bc18e4add2ddef8f465e34170d0b0a9200491c
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-07 02:05:28 +0600

    snippets

---
+++ index.tsx
@@ -0,0 +1,19 @@
+// settings => user snippets => new global snippets file then set code 
+ // .vscode/react.json
+// {
+// 	"react component": {
+// 	"prefix": "cc",
+// 	"body": [
+// 	"const ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/} = () => {",
+// 	" return (",
+// 	" <div>",
+// 	" <h2>Welcome to the ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/} page</h2>",
+// 	" </div>",
+// 	" );",
+// 	"};",
+// 	"",
+// 	"export default ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/};"
+// 	],
+// 	"description": "React Functional Component Template"
+// 	}
+// }
